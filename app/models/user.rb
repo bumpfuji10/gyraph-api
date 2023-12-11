daf6cd2b5c9623d7bd800ae5774d6b81d63a54be
Yo Kamada <bumpfuji10@gmail.com>
2023-12-11 23:13:28 +0900

    password_digestのvalidationを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
+  validates :password_digest, presence: true, length: { minimum: 8 }
   has_secure_password
 end
